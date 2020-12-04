ac6c0f56033c121a2ec61d027ad8f47c26749a25
Moien Tajik <moientajik@hotmail.com>
2020-12-04 16:12:51 +0330

    add required packages for source-generator

---
+++ src/Generators/GraphqlCodeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace GraphQL.Tools.Generators
+{
+    [Generator]
+    internal class GraphqlCodeGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
